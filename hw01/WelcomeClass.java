fd58c8e9ba7d2bef1f0273bc9c1914ec79ef3c60
cwadsworth <cjw218@lehigh.edu>
2015-01-28 03:26:23 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,28 @@
+// Christian Wadsworth
+// Homework 1
+// WelcomeClass Java Program
+//
+//  first compile the program
+//      javac WelcomeClass.java
+//  run the program
+//      java WelcomeClass//
+
+//  define a class
+public class WelcomeClass{
+    
+//  add main method
+    public static void main(String[] args) {
+// print the statement with box around it
+    System.out.println("-----------");
+    System.out.println("| WELCOME |");
+    System.out.println("-----------");
+    System.out.println("^  ^  ^  ^  ^  ^");
+    System.out.println("/ \\/ /\\ /\\ /\\ /\\ \\ ");
+    System.out.println("<-C--J--W--2--1--8->");
+    System.out.println("\\ /\\ /\\ /\\ /\\ /\\ /");
+    System.out.println(" v  v  v  v  v  v");
+    System.out.println("Last semester I entered a 7 game invitational");
+    System.out.println("chess tournament. It was at a club in Bethlehem.");
+    
+}
+}

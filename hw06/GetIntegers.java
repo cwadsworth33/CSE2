6125df87e812e6113b6c5570bd94f3adc22ce984
cwadsworth <cjw218@lehigh.edu>
2015-03-04 03:05:28 +0000

    updated GetIntegers

--- hw06/GetIntegers.java
+++ hw06/GetIntegers.java
@@ -9,20 +9,23 @@ public class GetIntegers{ //public class GetIntegers
         Scanner myScanner; //Initialize scanner
         myScanner = new Scanner(System.in); //Create new scanner
         //Loop to take a positive integer from the user (only +)
-        int integer;
-        int sum=0;
-        System.out.println("Please enter 5 positive integers.");
-        for(int i=0; i<5; i++){
-            integer=myScanner.nextInt();
-            if(integer<0){
-                System.out.println("Only positive numbers");
-                i=0;
+        int integer=0;//initialize integer as 0
+        int sum=0;//initialize sum as 0
+        System.out.println("Please enter 5 positive integers.");//Prompt user
+        for(int i=0; i<5; i++){//for loop to repeat action exactly 5 times
+            integer=myScanner.nextInt();//reassign integer to scanner from user 
+
+            
+            
+            if(integer<0){//throws out negative numbers
+                System.out.println("Invalid input");
+                i=-1;
                 sum=0;
             }
-            sum=sum+integer;
+            sum=sum+integer;//adds up all integers
 
         }
-        System.out.println("Sum:"+sum);
+        System.out.println("Sum:"+sum);//final print
         
         
         

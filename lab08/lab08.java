da23ebef398a4b93b3734860821625ec6258ead0
cwadsworth <cjw218@lehigh.edu>
2015-03-26 02:48:49 +0000

    updated HW08

---
+++ lab08/lab08.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;//import scanner
+public class waves{//create class
+    public static void main(String[] args){//main method
+    Scanner myScanner;//initialize scanner myscanner
+    myScanner = new Scanner(System.in);//create new scanner
+    int n=myScanner.nextInt();
+    factorial(n);
+    print(sum);
+    
+    }
+public static int factorial(int n){
+    
+    int product = 1;
+    int sum=0;
+    
+    while(n>0){
+        int c=n;
+        while(c>0){
+        product=product*n;
+        c--;
+    }
+    sum=sum+product;
+    n--;
+    return sum;
+    }
+    
+public static int print(int sum){
+    System.out.println("The super factorial is "+sum);
+}
+    
+    
+}
+}
